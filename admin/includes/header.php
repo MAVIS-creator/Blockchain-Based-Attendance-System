6c2d605f50447e742f96bcc9c17e0765e36f4fae
MAVIS-creator <akintunde.dolapo1@gmail.com>
2025-11-07 13:03:03 +0100

    Auto-save 07/11/2025 13:03:03.66

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -39,8 +39,8 @@
         <link rel="icon" type="image/x-icon" href=".../asset/favicon.ico">
   <link rel="apple-touch-icon" sizes="180x180" href=".../asset/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href=".../asset/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="../asset/favicon-16x16.png">
-  <link rel="manifest" href="../asset/site.webmanifest">
+  <link rel="icon" type="image/png" sizes="16x16" href=".../asset/favicon-16x16.png">
+  <link rel="manifest" href=".../asset/site.webmanifest">
 <link rel="stylesheet" href="<?= htmlspecialchars($adminRoot) ?>/admin-theme.css">
 <?php
   // expose CSRF token to JS by including the helper
